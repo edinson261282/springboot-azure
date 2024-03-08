45115804312afcdcdf69310e833b4bf6aa65570f
enarvaezo <edinsonnarvaez@gmail.com>
2024-03-07 23:51:45 -0500

    first commit

---
+++ src/main/java/stapp/maven/stapp/StappApplication.java
@@ -0,0 +1,13 @@
+package stapp.maven.stapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stapp/maven/stapp/StappApplicationTests.java
@@ -0,0 +1,13 @@
+package stapp.maven.stapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
