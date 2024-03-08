f031f15adf80f8fa26a884bca6b0441bc482062b
enarvaezo <edinsonnarvaez@gmail.com>
2024-03-08 00:15:45 -0500

    Ajuste variable de entorno

--- src/main/java/stapp/maven/stapp/controller/HealthCheckController.java
+++ src/main/java/stapp/maven/stapp/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package stapp.maven.stapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/health-check")
 public class HealthCheckController {
 
+    @Value("${stapp.url}")
+    private String url;
+
     @GetMapping("/")
     public ResponseEntity<?> index() {
-        return new ResponseEntity<>("Hola mundo!", HttpStatus.OK);
+        return new ResponseEntity<>("Hola mundo! " + this.url, HttpStatus.OK);
     }
 
 }
